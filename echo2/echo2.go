526e1792a743ca8c233c01ce13b71e80b7f38d08
Matt <m.toseland@validus-ivc.co.uk>
2018-03-12 10:52:26 +0000

    more examples

--- echo2/echo2.go
+++ echo2/echo2.go
@@ -3,14 +3,25 @@ package main
 
 import (
 	"fmt"
+	"math/big"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	s, sep := "", ""
+
 	for _, arg := range os.Args[1:] {
 		s += sep + arg
 		sep = " "
 	}
-	fmt.Println(s)
+
+	r := new(big.Int)
+	fmt.Println(r.Binomial(1000, 10))
+
+	secs := time.Since(start)
+	fmt.Println(s, secs)
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
