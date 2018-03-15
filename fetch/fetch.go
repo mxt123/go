4ad352e244d53b71afaeeb04667855f261cceaab
Matt <m.toseland@validus-ivc.co.uk>
2018-03-15 08:55:56 +0000

    more tutorials

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -3,23 +3,31 @@ package main
 
 import (
 	"fmt"
-	//"io/ioutil"
+	"io/ioutil"
 	"net/http"
 	"os"
-	"io"
+	//"io"
+	"strings"
 )
+
+var prefix = "http://"
 
 func main() {
 	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, prefix) {
+			url = prefix + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
-	//	b, err := ioutil.ReadAll(resp.Body)
+		b, err := ioutil.ReadAll(resp.Body)
 		fmt.Println(resp.Status)
-		b,err := io.Copy(os.Stdout,resp.Body)
-		fmt.Println(b)
+	//	b,err := io.Copy(os.Stdout,resp.Body)
+		var _ = b
+	//	fmt.Println(b)
 		resp.Body.Close()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
