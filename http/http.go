ad4674fd613f8aa26dd138f10dc234fca3226b59
Matt <m.toseland@validus-ivc.co.uk>
2018-03-27 13:43:08 +0100

    wip

--- http/http.go
+++ http/http.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
 var (
-	user     = flag.String("user", "user", "username")
-	password = flag.String("password", "password", "password")
+	user       = flag.String("user", "user", "username")
+	password   = flag.String("password", "password", "password")
+	dbUser     = flag.String("dbUser", "validate", "username")
+	dbPassword = flag.String("dbPassword", "validate", "password")
+	server     = flag.String("server", "localhost", "Server to connect to")
+	dbName     = flag.String("dbname", "validate", "Database name")
 )
 
 func check(u, p string) bool {
 	if u == *user && p == *password {
 		return true
-	} else {
-		return false
-
 	}
+	return false
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -25,25 +31,48 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if !check(u, p) {
 		http.Error(w, "Unauthorised.", 401)
 		return
-	} else {
-		switch r.Method {
-		case "GET":
-			http.ServeFile(w, r, r.URL.Path[1:])
-		case "POST":
-			if err := r.ParseForm(); err != nil {
-				fmt.Fprintf(w, "ParseForm() err: %v", err)
-				return
-			}
-			for key, values := range r.Form {
-				for _, value := range values {
-					fmt.Fprint(w, key, ",", value)
-				}
+	}
+	switch r.Method {
+	case "POST":
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
+			return
+		}
+		for key, values := range r.Form {
+			for _, value := range values {
+				fmt.Fprint(w, key, ",", value)
 			}
 		}
 	}
 }
 
 func main() {
+
+	db, err := sql.Open("sqlserver", "server="+*server+";user id="+*dbUser+";password="+*dbPassword)
+
+	if err != nil {
+		fmt.Println("From Open() attempt: " + err.Error())
+	}
+	defer db.Close()
+
+	fmt.Println("connected")
+
+	var rows, _ = db.Query("select data from validate.dbo.spam")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var val string
+		if err := rows.Scan(&val); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(val)
+	}
+
 	flag.Parse()
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8083", nil)
